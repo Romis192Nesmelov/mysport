6fe4cbabdee116a043b8be76d421564bbc310cf1
Romis <romis.nesmelov@gmail.com>
2021-04-22 16:49:18 +0300

    fixes layout

--- resources/views/_input_block.blade.php
+++ resources/views/_input_block.blade.php
@@ -17,7 +17,7 @@
 @endif
 <div class="form-group has-feedback has-feedback-left {{ count($errors) && $errors->has($name) ? "has-error" : '' }} {{ isset($addClass) ? $addClass : '' }}" {!! isset($attrString) ? $attrString : '' !!}>
     <input {{ isset($disabled) && $disabled ? 'disabled=disabled' : '' }} {{ (!isset($icon) || !$icon) && (!count($errors) || !$errors->has($name)) ? 'style=padding-left:10px' : '' }} {{ isset($min) && $min ? 'min='.$min : '' }} {{ isset($max) && $max ? 'max='.$max : '' }} name="{{ $name }}" type="{{ $type }}" class="form-control" placeholder="{{ isset($placeholder) && $placeholder ? $placeholder : '' }}" value="{{ isset($value) && !count($errors) ? $value : (Session::has($name) ? Session::get($name) : old($name)) }}">
-    @if (isset($icon) && $icon)
+    @if (isset($icon) && $icon && !count($errors))
         <div class="form-control-feedback">
             <i class="{{ $icon }} text-muted"></i>
         </div>
